467d3dfb23ef355b8da2391ac6b8e1cd5f4b1b11
Nathalia <nathimartins590@gmail.com>
2026-07-09 20:47:14 -0300

    tche

--- index.js
+++ index.js
@@ -1,150 +1,70 @@
 import express from 'express'
-import bancoDedados from './repository'
+import bancoDeDados from './repository/bancoDedados.js';
 
 const app = express()
 
-app.get("/api/v1/nome", (req, res) => {
-    const id = req.query.id
-    const nome = req.query.nome
+app.get('/api/v1/doguinho/:id', (req, res) => {
 
-    if(!id || !nome) {
-        req.status(400).send({ message: "nome invalido!" })
-    }
-
-    const cachorro = {
-        id,
-        nome
-    }
-
-    bancoDedados.push(cachorro)
-
-    res.status(200).send({ message: "doguinho adicionado com sucesso!"})
-})
-
-app.get("/api/v1/raça", (req, res) => {
-    const id = req.query.id
-    const raça = req.query.raça
-
-    if(!id || !raça) {
-        req.status(400).send({ message: "raça de doguinho invalida!"})
-    }
-
-    const cachorro = {
-        id,
-        raça
-    }
-
-    bancoDedados.push(cachorro)
-
-    res.status(200).send({ message: "raça do doguinho adicionada com sucesso!"})
-})
-
-app.get("/api/v1/idade", (req, res) => {
-    const id = req.query.id
-    const idade = req.query.idade
-
-    if(!id || !idade) {
-        req.status(400).send({ message: "idade do doguinho invalida!"})
-    }
-
-    const cachorro = {
-        id,
-        idade
-    }
-
-    bancoDedados.push(cachorro)
-
-    res.status(200).send({ message: "idade do doguinho adicionada com sucesso!"})
-})
-
-app.get("/api/nome1", (req, res) => {
-    res.send({ cachorro: bancoDeDados })
-})
-
-app.get("/api/raça1", (req, res) => {
-    res.send({ cachorro: bancoDeDados })
-})
-
-app.get("/api/idade1", (req, res) => {
-    res.send({ cachorro: bancoDeDados })
-})
-
-app.get("/api/nome2/:id", (req, res) => {
     const id = req.params.id
-    const cachorro = bancoDeDados.find(it => it.id == id)
+    const cachorro = bancoDedados.find(it => it.id == id)
+
     if (!cachorro) {
-        res.send({ message: "doguinho não encontrado" })
+
+        res.send({ message: "doguinho nao encontrado  " })
+
         return
     }
     res.send({ cachorro })
 })
 
-app.get("/api/raça2/:id", (req, res) => {
+
+app.get('/api/v1/dog-delt/:id', (req, res) => {
+
     const id = req.params.id
-    const cachorro = bancoDeDados.find(it => it.id == id)
+    const cachorro = bancoDedados.find(it => it.id == id)
+
     if (!cachorro) {
-        res.send({ message: "doguinho não encontrado" })
+
+        res.send({ message: "doguinho nao encontrado  " })
+
         return
     }
-    res.send({ cachorro })
+    res.send({ cachorro }) = 0
 })
 
-app.get("/api/idade2/:id", (req, res) => {
-     const id = req.params.id
-    const cachorro = bancoDeDados.find(it => it.id == id)
+app.get('/api/v1/dog-edit/:id/:alterar', (req, res) => {
+
+    const id = req.params.id
+    const alterar = req.params.alterar
+    const cachorro = bancoDedados.find(it => it.id == id)
+
     if (!cachorro) {
-        res.send({ message: "doguinho não encontrado" })
+
+        res.send({ message: "doguinho nao encontrado  " })
+
         return
     }
-    res.send({ cachorro })
+    res.send({ name }) = alterar
 })
 
-app.get("/api/nome3/:id", (req, res) => {
-    const id = req.params.id
-    const { nome } = req.query
-    const cachorro = bancoDeDados.find(it => it.id == id)
-    if(!cachorro) {
-        res.send({ message: "Favor informar nome do doguinho" })
-        return 
+app.get('/api/dog', (req, res) => {
+
+    const { id, name } = req.query
+
+    bancoDedados.push({ id, nome })
+
+    console.log(bancoDedados)
+
+    if (!id || !name) {
+        res.send({ message: "Favor informar id e o nome do doguinho  " })
+        return
     }
-    cachorro.nome = nome
-    res.send({ message: "doguinho alterado com sucesso" })
+
+    res.send({ message: "Doguinho criado com sucesso! " })
+
 })
 
-app.get("/api/raça3/:id", (req, res) => {
-    const id = req.params.id
-    const { raça } = req.query
-    const cachorro = bancoDeDados.find(it => it.id == id)
-    if(!cachorro) {
-        res.send({ message: "Favor informar raça do doguinho" })
-        return 
-    }
-    cachorro.raça = raça
-    res.send({ message: "doguinho alterado com sucesso" })
-})
+app.listen(3000, () => {
+    console.log("servidor escutando 3000")
 
-app.get("/api/idade3/:id", (req, res) => {
-    const id = req.params.id
-    const { idade } = req.query
-    const cachorro = bancoDeDados.find(it => it.id == id)
-    if(!cachorro) {
-        res.send({ message: "Favor informar raça do doguinho" })
-        return 
-    }
-    cachorro.idade = idade
-    res.send({ message: "doguinho alterado com sucesso" })
 })
-
-app.get("/api/nome4/:id", (req, res) => {
-    const id = req.params.id
-    const cachorro = bancoDeDados.find(it => it.id == id)
-    if(!cachorro) {
-        res.send({ message: "Favor informar id e nome" })
-        return 
-    }
-    bancoDeDados.splice(it => it.id == id, 1)
-    res.send({ message: "registro deletado com sucesso" })
-})
-app.listen(3000,() => {
-    console.log("bah ta funcionando 3000")
-});
